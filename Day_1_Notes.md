0cf9c1b21be7f0e153b7cc8f18e8d94e88ba9a05
Sam Welch <samcwelch@gmail.com>
2021-11-18 14:54:18 -0500

    some notes on proposed updates

---
+++ Day_1_Notes.md
@@ -0,0 +1,19 @@
+Server: 
+
+to do: 
+  1. add database
+  2. unit tests for components/functions that are working properly
+  3. fix code and upgrade
+  4. E2E testing
+
+
+code fixes: 
+
+config: can we access Pedro's DB?
+controllers - cocktail: fix a few functions, comments in line
+redux: 
+  reduxApi - make dynamic
+  api call should dispatch appropriate action to set global liquor state
+
+General notes/questions: 
+- loadUser?
--- client/src/APIService/cocktails-db-api.js
+++ client/src/APIService/cocktails-db-api.js
@@ -86,50 +86,50 @@ apiService.getAllMyCocktails = async accessToken => {
 };
 
 // DID NOT USE DO FAR
-apiService.createCocktail = async (cocktail, accessToken) => {
-  return fetch(`${BASE_URL}/`, {
-    method: 'POST',
-    credentials: 'include',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify(cocktail),
-  })
-    .then(res => res.json())
-    .catch(err => console.log(err));
-};
+// apiService.createCocktail = async (cocktail, accessToken) => {
+//   return fetch(`${BASE_URL}/`, {
+//     method: 'POST',
+//     credentials: 'include',
+//     mode: 'cors',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     body: JSON.stringify(cocktail),
+//   })
+//     .then(res => res.json())
+//     .catch(err => console.log(err));
+// };
 
-// DID NOT USE DO FAR
-apiService.updateCocktail = async (id, accessToken) => {
-  return fetch(`${BASE_URL}/${id}`, {
-    method: 'PUT',
-    credentials: 'include',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify(body),
-  })
-    .then(res => res.json())
-    .catch(err => console.log(err));
-};
+// // DID NOT USE DO FAR
+// apiService.updateCocktail = async (id, accessToken) => {
+//   return fetch(`${BASE_URL}/${id}`, {
+//     method: 'PUT',
+//     credentials: 'include',
+//     mode: 'cors',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     body: JSON.stringify(body),
+//   })
+//     .then(res => res.json())
+//     .catch(err => console.log(err));
+// };
 
-// DID NOT USE DO FAR
-apiService.deleteCocktail = async (id, accessToken) => {
-  return await fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE',
-    credentials: 'include',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-    .then(res => res.json())
-    .catch(err => console.log(err));
-};
+// // DID NOT USE DO FAR
+// apiService.deleteCocktail = async (id, accessToken) => {
+//   return await fetch(`${BASE_URL}/${id}`, {
+//     method: 'DELETE',
+//     credentials: 'include',
+//     mode: 'cors',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//   })
+//     .then(res => res.json())
+//     .catch(err => console.log(err));
+// };
 
 export default apiService;
